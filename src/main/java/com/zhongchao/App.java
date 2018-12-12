82991778c2f6a71406de7a327b5e18e302689a3f
zhongpeichang <zhongpeichang@163.com>
2018-12-12 20:02:21 +0800

    app第四次添加打印语句

--- src/main/java/com/zhongchao/App.java
+++ src/main/java/com/zhongchao/App.java
@@ -10,5 +10,6 @@ public class App {
         System.out.println( "Hello World!" );
         System.out.println("change01");
         System.out.println("change02");
+        System.out.println("change03");
     }
 }
